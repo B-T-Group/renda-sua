1656c67dc18b5135bdf8b24677f2a581de90b9aa
Samuel Besong <besongsamuel@yahoo.com>
2025-06-25 12:08:30 -0400

    remove frontend app

--- apps/frontend/src/config/api.config.ts
+++
@@ -1,8 +0,0 @@
-export const apiConfig = {
-  backend: {
-    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000',
-  },
-  hasura: {
-    baseURL: process.env.REACT_APP_HASURA_URL || 'http://localhost:8080/v1/graphql',
-  },
-}; 
