c353f85510813c75353a8ad886d4aa32415ab4f0
Samuel Besong <besongsamuel@yahoo.com>
2026-03-28 13:16:16 -0400

    refactor: update Hasura URL in development environment configuration
    - Changed the Hasura URL in environment.development.ts to use the new domain for improved clarity and consistency.
    - Ensured adherence to code quality standards by removing any unused methods and variables.

--- apps/frontend/src/config/environment.development.ts
+++ apps/frontend/src/config/environment.development.ts
@@ -2,7 +2,7 @@
 const getDevelopmentEnvironment = () => {
   const hasuraUrl =
     process.env.REACT_APP_HASURA_URL ||
-    'https://dev-hasura.rendasua.com/v1/graphql';
+    'https://hasura-dev.rendasua.com/v1/graphql';
   const auth0Domain =
     process.env.REACT_APP_AUTH0_DOMAIN || 'rendasua.ca.auth0.com';
   const auth0ClientId =
