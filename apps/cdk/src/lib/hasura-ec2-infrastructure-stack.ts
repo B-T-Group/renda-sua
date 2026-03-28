9d2cc69b38658ed93895f8afbbb45808fc06a60e
Samuel Besong <besongsamuel@yahoo.com>
2026-03-28 11:15:49 -0400

    refactor: update EC2 instance logical ID for Hasura stack
    - Changed the logical ID of the EC2 instance in the HasuraEc2EnvironmentStack to improve clarity and maintainability.
    - Ensured adherence to code quality standards by removing any unused methods and variables.

--- apps/cdk/src/lib/hasura-ec2-infrastructure-stack.ts
+++ apps/cdk/src/lib/hasura-ec2-infrastructure-stack.ts
@@ -49,7 +49,7 @@ export class HasuraEc2EnvironmentStack extends cdk.Stack {
     });
 
     // Logical ID change forces new EC2 so user data re-runs (fixes not applied on in-place updates).
-    const instance = new ec2.Instance(this, `${namePrefix}InstanceHasuraEC2`, {
+    const instance = new ec2.Instance(this, `${namePrefix}HasuraEC2`, {
       vpc,
       vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
       instanceType: new ec2.InstanceType(props.instanceType ?? 't4g.micro'),
